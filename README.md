b4ca8f49b48e0bfa26f5417ad9b0ba1908c627b8
Ivan Jovanovic <ij.smartnet@gmail.com>
2018-04-22 00:07:50 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ijgithub.github.io
+# 4096
+
+4096 is a fork of the popular [2048](http://gabrielecirulli.github.io/2048/) game with the following key differences:
+
+ * the goal is to get to the *4096* tile
+ * the grid is *5x5*
+ * *2 tiles* are added each round instead of 1
+
+[Play it here!](http://mattjmattj.github.io/4096/)
+
+## License
+2048 and 4096 are licensed under the [MIT license.](https://github.com/mattjmattj/4096/blob/master/LICENSE.txt)
---
+++ js/local_score_manager.js
@@ -0,0 +1,56 @@
+window.fakeStorage = {
+  _data: {},
+
+  setItem: function (id, val) {
+    return this._data[id] = String(val);
+  },
+
+  getItem: function (id) {
+    return this._data.hasOwnProperty(id) ? this._data[id] : undefined;
+  },
+
+  removeItem: function (id) {
+    return delete this._data[id];
+  },
+
+  clear: function () {
+    return this._data = {};
+  }
+};
+
+function LocalScoreManager() {
+  this.key     = "4096bestScore";
+  this.currentBoardState     = "4096currentBoardState";
+
+  var supported = this.localStorageSupported();
+  this.storage = supported ? window.localStorage : window.fakeStorage;
+}
+
+LocalScoreManager.prototype.localStorageSupported = function () {
+  var testKey = "test";
+  var storage = window.localStorage;
+
+  try {
+    storage.setItem(testKey, "1");
+    storage.removeItem(testKey);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+LocalScoreManager.prototype.get = function () {
+  return this.storage.getItem(this.key) || 0;
+};
+
+LocalScoreManager.prototype.set = function (score) {
+  this.storage.setItem(this.key, score);
+};
+
+LocalScoreManager.prototype.getState = function () {
+  return this.storage.getItem(this.currentBoardState) || false;
+};
+
+LocalScoreManager.prototype.setState = function (state) {
+  this.storage.setItem(this.currentBoardState, JSON.stringify(state));
+};
